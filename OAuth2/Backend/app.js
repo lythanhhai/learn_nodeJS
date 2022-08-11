e3bff1cd92bfa9886b39f7f6fb69e244b9ab9e19
lythanhhai <lythanhhait@gmail.com>
2022-08-11 22:47:01 +0700

    handle OAuth2 with passport

--- OAuth2/Backend/app.js
+++ OAuth2/Backend/app.js
@@ -1,10 +1,29 @@
-require("dotenv").require
-const express = require('express');
+require("dotenv").config();
+require("./auth");
+const express = require("express");
 const app = express();
+const passport = require('passport')
+var userProfile;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.get('/success', (req, res) => res.send(userProfile));
+// app.get('/error', (req, res) => res.send("error logging in"));
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] }))
+//   res.status(200).json({ success: true })
+
+app.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: 'http://localhost:3000' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    console.log(res)
+    console.log(req)
+    res.status(200).send("<p>o kia</p>")
+  });
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server running at ${process.env.PORT}`);
-})
+  console.log(`Server running at ${process.env.PORT}`);
+});
--- OAuth2/frontend/src/App.js
+++ OAuth2/frontend/src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import axios from "axios";
 function App() {
   return (
     <div className="App">
-      <button type="button" className="button" onclick={() => {}}>
+      {/* <button
+        type="button"
+        className="button"
+        onClick={() => {
+          axios
+            .get("http://localhost:5000/auth/google")
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        }}
+      >
         Login with Google
-      </button>
+      </button> */}
+      <a href="http://localhost:5000/auth/google">Login with google</a>
     </div>
   );
 }
